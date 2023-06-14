e12d5bdcbd50073c131646bef509177043c3abdf
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-14 06:51:56 -0700

    Changed callback to promise

--- Ch01/01_02/start/index.js
+++ Ch01/01_02/start/index.js
@@ -1,9 +1,19 @@
-function delay(seconds, callback) {
-  setTimeout(callback, seconds * 1000);
-}
+// Resolving promises
+var delay = (seconds) =>
+  new Promise((resolve, reject) => {
+    setTimeout(resolve, seconds * 1000);
+  });
 
-delay(1, () => {
-  console.log("one second");
+// function delay(seconds, callback) {
+//   setTimeout(callback, seconds * 1000);
+// }
+
+delay(1).then(() => {
+  console.log("The delay has ended");
 });
+
+// delay(1, () => {
+//   console.log("one second");
+// });
 
 console.log("end first tick");
