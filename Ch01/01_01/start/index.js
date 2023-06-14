f671e7b75389f77fea677ccde86992903f5a8ca9
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-14 05:46:31 -0700

    Set timer example

--- Ch01/01_01/start/index.js
+++ Ch01/01_01/start/index.js
@@ -1,12 +1,21 @@
-function hideString(str, done) {
-  process.nextTick(() => {
-    // now done is called asynchronously in the next tick, a true callback
-    done(str.replace(/[a-zA-Z]/g, "X"));
-  });
+// function hideString(str, done) {
+//   process.nextTick(() => {
+//     // now done is called asynchronously in the next tick, a true callback
+//     done(str.replace(/[a-zA-Z]/g, "X"));
+//   });
+// }
+
+// hideString("Hello World", (hidden) => {
+//   console.log(hidden);
+// });
+
+// console.log("End");
+let time = 5;
+
+function delay(time, callback) {
+  setTimeout(callback, time * 1000);
 }
 
-hideString("Hello World", (hidden) => {
-  console.log(hidden);
+delay(time, () => {
+  console.log(`${time} seconds delay`);
 });
-
-console.log("End");
