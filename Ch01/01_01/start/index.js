020102032a4c8f2f9caff2b9a0313ff10d3898e9
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-14 05:29:08 -0700

    Call done asynchronously in next tick

--- Ch01/01_01/start/index.js
+++ Ch01/01_01/start/index.js
@@ -1,5 +1,8 @@
 function hideString(str, done) {
-  done(str.replace(/[a-zA-Z]/g, "X"));
+  process.nextTick(() => {
+    // now done is called asynchronously in the next tick, a true callback
+    done(str.replace(/[a-zA-Z]/g, "X"));
+  });
 }
 
 hideString("Hello World", (hidden) => {
