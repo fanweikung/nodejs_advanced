923d96b56fc220ee62275e9ef34f845bba6b1207
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-15 14:25:15 -0700

    Another Ex: writeFile callback

--- Ch01/01_04/start/index.js
+++ Ch01/01_04/start/index.js
@@ -1,18 +1,13 @@
+var fs = require("fs");
 var { promisify } = require("util");
 
-var delay = (seconds, callback) => {
-  if (seconds > 3) {
-    callback(new Error(`${seconds} seconds it too long!`));
-  } else {
-    setTimeout(
-      () => callback(null, `the ${seconds} second delay is over.`),
-      seconds
-    );
+var writeFile = fs.writeFile(
+  "callback.txt",
+  "hello from callback writefile",
+  (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("file saved");
   }
-};
-
-// convert callback into promise
-var promiseDelay = promisify(delay);
-promiseDelay(4)
-  .then(console.log)
-  .catch((error) => console.log(`error: ${error.message}`));
+);
