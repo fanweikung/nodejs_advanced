5aaa237ca9a9ed436bd801f62be02c3c2f118fd9
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-15 04:54:58 -0700

    Handle error using catch method

--- Ch01/01_03/start/index.js
+++ Ch01/01_03/start/index.js
@@ -1,13 +1,16 @@
 var delay = (seconds) =>
   new Promise((resolves, rejects) => {
+    throw new Error("argh");
+
     setTimeout(() => {
       resolves("the long delay has ended");
-    }, time);
+    }, seconds * 1000);
   });
 
 delay(1)
   .then(console.log)
   .then(() => 42)
-  .then((number) => console.log("Hello world: ${number}"));
+  .then((number) => console.log(`Hello world: ${number}`))
+  .catch((error) => console.log(`error: ${error.message}`));
 
 console.log("end first tick");
