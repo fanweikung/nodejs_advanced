c95d9722da15199809333033c1e0d40648b600f5
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-17 04:32:26 -0700

    Added delay and unlink example

--- Ch01/01_07/start/index.js
+++ Ch01/01_07/start/index.js
@@ -13,6 +13,10 @@ Promise.all([
   writeFile("Readme.md", "Hello World"),
   writeFile("Readme.txt", "Hello World"),
   writeFile("Readme.json", '{ "Hello": "World" }'),
+  delay(2), // finish writeFile before unlink
+  unlink("Readme.md"),
+  unlink("Readme.txt"),
+  unlink("Readme.json"),
 ])
   .then(() => readdir(__dirname))
   .then(console.log);
