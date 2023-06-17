64405a91bf251f3265b8251ab57f385d4ac050e0
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-16 17:19:37 -0700

    Obtain data using readdir as a promise

--- Ch01/01_06/start/index.js
+++ Ch01/01_06/start/index.js
@@ -2,33 +2,16 @@ var fs = require("fs");
 var { promisify } = require("util");
 var writeFile = promisify(fs.writeFile);
 var unlink = promisify(fs.unlink);
+var readdir = promisify(fs.readdir);
 var beep = () => process.stdout.write("\x07");
 var delay = (seconds) =>
   new Promise((resolves) => {
     setTimeout(resolves, seconds * 1000);
   });
 
-const doStuffSequentially = async () => {
-  console.log("starting");
-  await delay(1);
-  console.log("waiting");
-  await delay(2);
-  try {
-    await writeFile("file.txt", "Sample File...");
-    beep();
-  } catch (error) {
-    console.error(error);
-  }
-  console.log("file.txt created");
-  await delay(3);
-  await unlink("file.txt");
-  beep();
-  console.log("file.txt removed");
+async function start() {
+  var files = await readdir(__dirname); //readdir is a promise
+  console.log(files); //gets executed after the promise is resolved
+}
 
-  return Promise.resolve();
-};
-
-doStuffSequentially()
-  .then(() => console.log("again again!!!"))
-  .then(() => doStuffSequentially())
-  .then(() => console.log("enough already..."));
+start();
