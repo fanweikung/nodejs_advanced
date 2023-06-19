f6139703ef753155bb5021a2a010d67391d7f061
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-19 02:47:14 -0700

    Stream the arrary as an object

--- Ch02/02_02/start/index.js
+++ Ch02/02_02/start/index.js
@@ -12,13 +12,17 @@ const peaks = [
 
 class StreamFromArray extends Readable {
   constructor(array) {
-    super({ encoding: "UTF-8" }); //set option
+    super({ objectMode: true }); //set option
     this.array = array;
     this.index = 0;
   }
   _read() {
     if (this.index <= this.array.length) {
-      const chunk = this.array[this.index];
+      //const chunk = this.array[this.index];
+      const chunk = {
+        data: this.array[this.index],
+        index: this.index,
+      };
       this.push(chunk);
       this.index += 1;
     } else {
