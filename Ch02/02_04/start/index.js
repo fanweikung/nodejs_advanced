f935f5ece223d96212aa066584969914cfe132a3
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-20 04:41:28 -0700

    Added writable stream to create a file in local dir

--- Ch02/02_04/start/index.js
+++ Ch02/02_04/start/index.js
@@ -1,8 +1,10 @@
-const { createReadStream } = require("fs");
+const { createReadStream, createWriteStream } = require("fs");
+
 const readStream = createReadStream("../../powder-day.mp4");
+const writeStream = createWriteStream("./copy.mp4");
 
 readStream.on("data", (chunk) => {
-  console.log("size: ", chunk.length);
+  writeStream.write(chunk);
 });
 
 readStream.on("error", (error) => {
@@ -10,5 +12,9 @@ readStream.on("error", (error) => {
 });
 
 readStream.on("end", () => {
-  console.log("done!");
+  writeStream.end();
+});
+
+writeStream.on("close", () => {
+  process.stdout.write("file copied\n"); //console log is a stdout
 });
