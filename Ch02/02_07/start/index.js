15d4cda4c28f6147bde8d3b0e47943f24e9ea9bc
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-20 08:01:49 -0700

    Added throttle to slowdown

--- Ch02/02_07/start/index.js
+++ Ch02/02_07/start/index.js
@@ -1,9 +1,27 @@
-const { PassThrough } = require("stream");
-const { createReadStream, createWriteStream } = require("fs");
+const { Duplex, PassThrough } = require("stream");
+const { createReadStream, createWriteStream, mkdirSync } = require("fs");
+const { PerformanceMeasure } = require("perf_hooks");
 
 const readStream = createReadStream("../../powder-day.mp4");
 const writeStream = createWriteStream("./copy.mp4");
 
+class Throttle extends Duplex {
+  constructor(ms) {
+    super();
+    this.delay = ms;
+  }
+  _write(chunk, encoding, callback) {
+    this.push(chunk);
+    setTimeout(callback, this.delay);
+  }
+  _read() {}
+
+  _final() {
+    this.push(null);
+  }
+}
+
+const throttle = new Throttle(10);
 const report = new PassThrough();
 
 var total = 0;
@@ -11,4 +29,4 @@ report.on("data", (chunk) => {
   total += chunk.length;
   console.log("bytes: ", total);
 });
-readStream.pipe(report).pipe(writeStream);
+readStream.pipe(throttle).pipe(report).pipe(writeStream);
